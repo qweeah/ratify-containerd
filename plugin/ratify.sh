defcb26956f94802d491dc76abc8371eeb4fd165
Billy Zha <jinzha1@microsoft.com>
2025-05-16 04:24:23 +0000

    chore: output configuration
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- plugin/ratify.sh
+++ plugin/ratify.sh
@@ -7,6 +7,14 @@ if [ ! -f ~/.ratify/config.json ]; then
     echo "ratify config file not found. failing open"
     exit 0
 fi
+
+echo "=== input ==="
+echo "1: $1"
+echo "2: $2"
+echo "3: $3"
+echo "4: $4"
+echo "==="
+
 ratifyOutput=$(~/.ratify/bin/ratify verify -c ~/.ratify/config.json -s $name --digest $digest)
 isSuccess=$(echo $ratifyOutput | jq .isSuccess)
 echo $ratifyOutput | jq . 
